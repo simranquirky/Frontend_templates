3ac4be15d8ae20cbeb435433fbf27fa348518842
Chetana16032002 <91176005+Chetana16032002@users.noreply.github.com>
2021-10-04 08:33:20 +0530

    Add files via upload

---
+++ Blog site/food-blog/assets/js/main.js
@@ -0,0 +1,58 @@
+/*
+ jQuery;
+ */
+
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	});
+
+
+
+
+	jQuery('.scrollup').click(function () {
+		$("html, body").animate({scrollTop: 0}, 2000);
+		return false;
+	});
+
+	jQuery('.nav a').bind('click', function () {
+		$('html , body').stop().animate({
+			scrollTop: $($(this).attr('href')).offset().top - 80
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+	
+	
+	jQuery(window).scroll(function () {
+	  var top = jQuery(document).scrollTop();
+		var height = 300;
+	  //alert(batas);
+	  
+	  if (top > height) {
+		jQuery('.navbar-fixed-top').addClass('menu-scroll');
+	  } else {
+	   jQuery('.navbar-fixed-top').removeClass('menu-scroll');
+	  }
+	});	
+ // Scroll up 
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+    });
+    $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+        return false;
+    }); 
+ 
+ new WOW().init();
+
+});
