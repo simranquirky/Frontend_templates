fe1554d9374c588f1024a2d63661bad8c6045503
Soumya Banerjee <soumyabanerjee783@gmail.com>
2021-10-04 17:16:27 +0530

    added section all-time

--- Social Media Dashboard/js/script.js
+++ Social Media Dashboard/js/script.js
@@ -1,10 +1,12 @@
 const themeIconLight = document.querySelector(".theme-toggle-light");
 const themeIconDark = document.querySelector(".theme-toggle-dark");
 const text = document.querySelectorAll(".text");
+const dataBox = document.querySelectorAll(".data");
 
 themeIconLight.addEventListener("click", () => {
   document.body.classList.add("body-dark");
   text.forEach((txt) => txt.classList.add("text-dark"));
+  dataBox.forEach((dt) => dt.classList.add("data-bg"));
   themeIconLight.classList.add("d-none");
   themeIconDark.classList.remove("d-none");
 });
@@ -12,8 +14,7 @@ themeIconLight.addEventListener("click", () => {
 themeIconDark.addEventListener("click", () => {
   document.body.classList.remove("body-dark");
   text.forEach((txt) => txt.classList.remove("text-dark"));
+  dataBox.forEach((dt) => dt.classList.remove("data-bg"));
   themeIconLight.classList.remove("d-none");
   themeIconDark.classList.add("d-none");
 });
-
-console.log("Hello");
