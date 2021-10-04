94347869d69502935e47ae49f02c4fa78bd9066e
Soumya Banerjee <soumyabanerjee783@gmail.com>
2021-10-04 19:37:41 +0530

    final changes

--- Social Media Dashboard/js/script.js
+++ Social Media Dashboard/js/script.js
@@ -2,11 +2,15 @@ const themeIconLight = document.querySelector(".theme-toggle-light");
 const themeIconDark = document.querySelector(".theme-toggle-dark");
 const text = document.querySelectorAll(".text");
 const dataBox = document.querySelectorAll(".data");
+const dataTodayBox = document.querySelectorAll(".today-data");
 
 themeIconLight.addEventListener("click", () => {
   document.body.classList.add("body-dark");
   text.forEach((txt) => txt.classList.add("text-dark"));
   dataBox.forEach((dt) => dt.classList.add("data-bg"));
+  dataTodayBox.forEach((dta) => {
+    dta.classList.add("data-bg");
+  });
   themeIconLight.classList.add("d-none");
   themeIconDark.classList.remove("d-none");
 });
@@ -15,6 +19,7 @@ themeIconDark.addEventListener("click", () => {
   document.body.classList.remove("body-dark");
   text.forEach((txt) => txt.classList.remove("text-dark"));
   dataBox.forEach((dt) => dt.classList.remove("data-bg"));
+  dataTodayBox.forEach((dta) => dta.classList.remove("data-bg"));
   themeIconLight.classList.remove("d-none");
   themeIconDark.classList.add("d-none");
 });
