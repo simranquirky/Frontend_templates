eb70386a3db206a0f22a44d56b6c9cc380ca5549
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2021-10-09 02:22:12 +0530

    Update Login Page
    Style, Add Insta logo, Add Username textbox, Password and Login button
    Add login with facebook and Forgot passoword texts

--- Instagram Clone/instagram-clone-frontend/src/components/login/Login.jsx
+++ Instagram Clone/instagram-clone-frontend/src/components/login/Login.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import InstaLoginGif from '../../img/instalogin.gif'
+import './Login.css'
+import InstaLoginGif from '../../img/InstaLoginPage.svg'
+import InstaLogo from '../../img/instalogo.png'
 
 function Login (){
 
     return(
         <>
             <Grid container>
-                <Grid item xs={3}>sdasd</Grid>
+                <Grid item xs={3}></Grid>
                 <Grid item xs={6}>
-                    <div>
+                    <div className="Instalogin-container">
                         {/*Image for login*/}
                         <div>
-                            <img src={InstaLoginGif} alt="loginimg" width="420px"></img>
+                            <img src={InstaLoginGif} alt="loginimg" width="454px"></img>
                         </div>
-                        <div></div>
+                        <div>
+                            <div className="Instalogin-RCompo">
+                                <img className="Instalogin-logo" src={InstaLogo} alt="InstaLogo"/>
+                                <div className="Instalogin-SignIn">
+                                    <input type="text" className="Instalogin-Text"/>
+                                    <input type="password" className="Instalogin-Text"/>
+                                    <button className="Instalogin-SignInButton">Log In</button>
+
+                                    <div className="Instalogin-ORdiv">
+                                        <div className="Instalogin-divider"></div>
+                                        <div className="Instalogin-OR">OR</div>
+                                        <div className="Instalogin-divider"></div>
+                                    </div>
+
+                                    <div className="Instalogin-Fb">Log in with Facebook</div>
+                                    <div className="Instalogin-Forgot">Forgot password?</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </Grid>
-                <Grid item xs={3}>dsadad</Grid>
+                <Grid item xs={3}></Grid>
             </Grid>
         </>
     );
