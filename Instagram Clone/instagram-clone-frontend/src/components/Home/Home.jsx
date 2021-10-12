144d15c909fa6431a5bbde27dc5234c2a1d2735d
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2021-10-12 12:55:48 +0530

    Create navbar of insta

--- Instagram Clone/instagram-clone-frontend/src/components/Home/Home.jsx
+++ Instagram Clone/instagram-clone-frontend/src/components/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import './Home.css';
 import Navbar from '../Navbar/Navbar.jsx';
+import MainContent from '../MainContent/MainContent.jsx';
 
 function Home(){
 
     return(
-        <>
+        <div className="home-container">
             <Navbar/>
-        </>
+            <MainContent/>
+        </div>
     );
 }
 
