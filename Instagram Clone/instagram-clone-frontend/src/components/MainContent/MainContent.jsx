12610295050b8099553d29a4227df03f93f929e6
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2021-10-14 00:53:23 +0530

    Create StoryBar component

--- Instagram Clone/instagram-clone-frontend/src/components/MainContent/MainContent.jsx
+++ Instagram Clone/instagram-clone-frontend/src/components/MainContent/MainContent.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './MainContent.css';
+import {Grid} from '@mui/material';
 
 function MainContent(){
 
     return(
         <>
-        
+            <Grid container>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={6}>
+                    <div></div>
+                </Grid>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={2}></Grid>
+            </Grid>
         </>
     );
 }
---
+++ Instagram Clone/instagram-clone-frontend/src/components/StoryBar/StoryBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function StoryBar(){
+
+    return(
+        <>
+        
+        </>
+    );
+}
+
+export default StoryBar;
