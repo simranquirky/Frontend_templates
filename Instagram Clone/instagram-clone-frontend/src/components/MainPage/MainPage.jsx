aee932be431b36fd201980b373990440833a468a
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2021-10-16 00:44:17 +0530

    Complete Post component

--- Instagram Clone/instagram-clone-frontend/src/components/MainPage/MainPage.jsx
+++ Instagram Clone/instagram-clone-frontend/src/components/MainPage/MainPage.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Post from '../Post/Post.jsx';
 import './MainPage.css';
 
-function MainPage(){
+function MainPage(props){
+
+    const [posts] = useState([
+
+        {
+            "username":"pizzahut",
+            "image":"https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGl6emElMjBzbGljZXxlbnwwfHwwfHw%3D&w=1000&q=80" ,
+            "likes":"235"
+        },
+        {
+            "username":"pizzahut",
+            "image":"https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGl6emElMjBzbGljZXxlbnwwfHwwfHw%3D&w=1000&q=80" ,
+            "likes":"235"
+        }
+    ])
+
 
     return(
         <div>
-           post
+            {
+                posts.map((item,index) =>{
+                    return(
+                        <Post 
+                            username={item.username} 
+                            image={item.image}
+                            likes={item.likes}
+                        />
+                    );
+                })
+            }
+
         </div>
     );
 }
--- Instagram Clone/instagram-clone-frontend/src/components/Post/Post.jsx
+++ Instagram Clone/instagram-clone-frontend/src/components/Post/Post.jsx
@@ -1,10 +1,73 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Post.css';
+import { Avatar } from '@mui/material';
+import Like from '../../img/love.svg';
+import Comment from '../../img/comment.svg';
+import Share from '../../img/share.svg';
 
-export default function Post() {
+export default function Post(props) {
+
+    const [commentList] = useState([
+
+        {
+            "username":"gayan",
+            "cmntID":"0001",
+            "timestamp":"1234",
+            "desc":"This is a comment"  
+        },
+        {
+            "username":"lakshitha",
+            "cmntID":"0001",
+            "timestamp":"1234",
+            "desc":"This is a comment"  
+        },
+        {
+            "username":"mahela87",
+            "cmntID":"0001",
+            "timestamp":"1234",
+            "desc":"This is a comment"  
+        }
+    ]);
+
+
     return (
-        <div>
-            
+        <div className="post-container">
+            {/* Header */}
+            <div className="post-header">
+                <Avatar className="post-dp" src="" />
+                <dev className="post-username">{props.username}</dev>
+            </div>
+
+            {/* Image */}
+            <div>
+                <img  className="post-image" src={props.image} alt="A Post"/>
+            </div>
+
+            {/* Analytics */}
+            <div>
+                <diV style={{"marginLeft":"10px"}}>
+                    <img className="post-reaction" src={Like} alt="Like" />
+                    <img className="post-reaction" src={Comment} alt="Comment" />
+                    <img className="post-reaction" src={Share} alt="Share" />
+                </diV>
+                <div style={{"fontWeight":"Bold", "marginLeft":"20px"}}>
+                    {props.likes} likes
+                </div>
+            </div>
+
+            {/* Comment Section  */}
+            <div>
+                {
+                    commentList.map((item, index) => {
+                        return(
+                            <div className="post-comment"><b>{item.username}</b> {item.desc}</div>
+                        )
+                        
+                    })
+                }
+
+                <input type="text" className="post-commentbox" placeholder="Add a comment..."/>
+            </div>
         </div>
     )
 }
